4e62f753d3d8a42b602d9b7131eaa5eab4f5a3d0
rtocastro <ricktorres@live.com>
2024-01-28 11:45:47 -0800

    added readme and screenshot

--- api/notes.js
+++ api/notes.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const router = require('express').Router();
-const db = require('../db/db.json')
 const fs = require("fs")
 const { v4: uuidv4 } = require('uuid')
+const db = require('../db/db.json')
 
 const newUuid = uuidv4();
 
@@ -20,32 +20,34 @@ router.post('/notes', (req, res) => {
 
     fs.writeFile('../db/db.json', JSON.stringify(db), (err) =>
         err ? console.error(err) : console.info(`\nData written to ${destination}`))
+        console.log(db)
         res.json("success")
 
 })
 
-// router.delete('/notes/:id', (req, res) => {
-
-//     var oldData = fs.readFileSync("./db/db.json", "utf8");
-
-//     const delDb = db.filter((note) =>
-//     note.id !== req.params.id)
-
-//     fs.writeFileSync('db/db.json', JSON.stringify(delDb))
-
-//     res.json(delDb)
-// });
-
-
 router.delete('/notes/:id', (req, res) => {
 
-    const oldData = fs.readFileSync("./db/db.json", "utf8");
-    const parsedData = JSON.parse(oldData);
-    const delDb = parsedData.filter((note) => note.id !== req.params.id);
+    var oldData = fs.readFileSync("./db/db.json", "utf8");
 
-    fs.writeFileSync('./db/db.json', JSON.stringify(delDb));
+    const delDb = db.filter((note) =>
+    note.id !== req.params.id)
 
-    res.json(delDb);
+    fs.writeFileSync('db/db.json', JSON.stringify(delDb))
+    res.json(delDb)
 });
+
+
+
+// router.delete('/notes/:id', (req, res) => {
+
+//     const oldData = fs.readFileSync("./db/db.json", "utf8");
+//     const parsedData = JSON.parse(oldData);
+//     const delDb = parsedData.filter((note) => note.id !== req.params.id);
+
+//     fs.writeFileSync('./db/db.json', JSON.stringify(delDb));
+
+
+//     res.json(delDb);
+// });
 
 module.exports = router;
---
+++ readme.md
@@ -0,0 +1,30 @@
+# Note Taker Application
+
+## Description
+
+Just an app that takes and saves notes and also brings them back up. 
+
+## Table of Contents (Optional)
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+
+## Installation
+
+Just VS code/Terminal to run the server and browser to view application
+
+## Usage
+
+You can use this app to keep track of tasks or to take notes on anything
+
+    ```md
+    ![screenshot](/public/assets/images/notepadscreenshot.png)
+    ```
+
+## Credits
+
+Myself, but I'd also like to credit BCS, My Tutor Jose Lopez, W3 schools, MDN and Stephanie Contreras
+
+
+
